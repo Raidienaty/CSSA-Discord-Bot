5b42b6d3d5d81daad17baa8b1af88e29af53fd5a
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2020-02-01 14:38:26 -0500

    Built basic help command

---
+++ CSSA_Discord_Bot/CSSA_Discord_Bot/Core/Commands/help.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Net;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSSA_Discord_Bot
+{
+    public class helpCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("help"), Alias("cmds", "HELP", "CMD"), Summary("Base help command, lists commands possible")]
+        public async Task help()
+        {
+            string[] commandNames = { "" };
+
+            EmbedBuilder build = new EmbedBuilder();
+
+            build.WithTitle("List of Commands");
+
+            build.AddField("help", "Lists the commands possible");
+
+            build.WithColor(Color.Blue);
+
+            await Context.Channel.SendMessageAsync("", false, build.Build());
+        }
+    }
+}
