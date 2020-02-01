e361687d51ccc15a929bafce759975d31b109cfa
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2020-02-01 14:06:01 -0500

    commit on master

---
+++ CSSA_Discord_Bot/CSSA_Discord_Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSSA_Discord_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
